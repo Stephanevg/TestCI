1c63a490813586a2a348737bf4f148f3991f81ce
stephanevg <svangulick@gmail.com>
2018-10-10 22:56:46 +0200

    show_var

--- CI/Start_CI.ps1
+++ CI/Start_CI.ps1
@@ -6,8 +6,12 @@ Function Get-AppveyorCIVariableValues {
     return $return
 }
 
-if($CommitMessage -match ".*show_var.*"){
-    Write-host "[VARIABLES] Exporting Variables"-ForegroundColor Green
+Write-host "[VARIABLES] Exporting Variables (outside)"-ForegroundColor Green
+Get-AppveyorCIVariableValues | Select Name,Value,Description | FT -AutoSize #| export-csv -path $ExportFile -NoTypeInformation
+
+if($env:APPVEYOR_REPO_COMMIT_MESSAGE -match ".*show_var.*"){
+    Write-host "[VARIABLES] Exporting Variables"-ForegroundColor DarkGreen
     #$ExportFile = join-Path -Path $BuildPath -ChildPath "CI_Variables.csv"
     Get-AppveyorCIVariableValues | Select Name,Value,Description | FT -AutoSize #| export-csv -path $ExportFile -NoTypeInformation
 }
+ 
