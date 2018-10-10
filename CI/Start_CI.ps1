c33aecd0fbc421c4170e26a9c43d0b698268bfac
stephanevg <svangulick@gmail.com>
2018-10-10 23:00:13 +0200

    update

--- CI/Start_CI.ps1
+++ CI/Start_CI.ps1
@@ -8,7 +8,7 @@ Function Get-AppveyorCIVariableValues {
 
 Write-host "[VARIABLES] Exporting Variables (outside)"-ForegroundColor Green
 Get-AppveyorCIVariableValues | Select Name,Value,Description | FT -AutoSize #| export-csv -path $ExportFile -NoTypeInformation
-
+Get-Variable | ? {$_.Name -like "CI*" -or $_.Name -like "APPVEYOR_*" -or $_.Name -like "CONFIGURATION" -OR $_.Name -like "PLATFORM"} Select Name,Value
 if($env:APPVEYOR_REPO_COMMIT_MESSAGE -match ".*show_var.*"){
     Write-host "[VARIABLES] Exporting Variables"-ForegroundColor DarkGreen
     #$ExportFile = join-Path -Path $BuildPath -ChildPath "CI_Variables.csv"
