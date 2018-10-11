e8d063fa3e4cfe6d541dd99bb56d7a69e9ff4071
stephanevg <svangulick@gmail.com>
2018-10-11 21:19:42 +0200

    update ci

--- CI/Start_CI.ps1
+++ CI/Start_CI.ps1
@@ -7,26 +7,12 @@ Function Get-AppveyorCIVariableValues {
 }
 
 
+
 if($env:APPVEYOR_REPO_COMMIT_MESSAGE -match ".*show_var.*"){
     Write-host "[VARIABLES] Exporting Variables"-ForegroundColor DarkGreen
     
     gci env:\ | sort Name | select Name,Value,Description
-   
+    
 }
 
-write-host "Vars: Local" -ForeGroundColor "Yellow"
-
-Get-Variable -Scope Local
-
-write-host "Vars: Script" -ForeGroundColor "red"
-
-Get-Variable -Scope Script
-
-write-host "Vars: Global" -ForeGroundColor "DarkMagenta"
-
-Get-Variable -Scope Global
-
-
-
-
- 
+gci env:\ | sort Name | select Name,Value,Description
